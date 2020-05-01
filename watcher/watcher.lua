836218e463171211f9a59870004405150d1ce02e
racherb <racherb@protonmail.com>
2020-05-01 02:01:13 -0400

    code: Remove start
    Remove start

--- watcher/watcher.lua
+++ watcher/watcher.lua
@@ -9,40 +9,6 @@
 -- @copyright Raciel Hernández 2019
 --
 
-local avro = require("avro_schema")
-
-local function start()
-    box.cfg{}
-
-    box.once('init', function()
-        --awatchers for Active Watchers
-        box.schema.create_space('awatchers')
-        box.space.awatchers:create_index(
-            "primary", {type = 'hash', parts = {1, 'unsigned'}}
-        )
-        box.space.awatchers:create_index(
-            "status", {type = "tree", parts = {2, 'str'}}
-        )
-    end)
-
-    local sch_ok, awatchers = avro.create(schema.awatchers)
-
-    if sch_ok then
-        -- compile models
-        local c_ok, c_awatcher = avro.compile(awatchers)
-        if c_ok then
-            awatcher_mod = c_awatcher
-            return true
-        else
-            log.error('Schema compilation failed')
-        end
-    else
-        log.info('Schema creation failed')
-    end
-
-    return false
-end
-
 local fw = require("file_watcher")
 
 local file = {
@@ -52,7 +18,6 @@ local file = {
 }
 
 return {
-    start = start,
     file = file
 }
 
