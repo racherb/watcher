7c220f3645b52c0f5b9a4df6baae066c3688579c
Unknown <unknown@example.com>
2021-01-30 23:21:16 -0300

    code: Comment types
    Comment types

--- watcher/types/file.lua
+++ watcher/types/file.lua
@@ -35,25 +35,25 @@ WATCHER.ITERATIONS = 10         --loops
 
 local FILE = {}
 
-FILE.NOT_YET_CREATED = '_'
-FILE.FILE_PATTERN = 'P'
-FILE.HAS_BEEN_CREATED = 'C'
-FILE.IS_NOT_NOVELTY = 'N'
-FILE.UNSTABLE_SIZE = 'U'
-FILE.UNEXPECTED_SIZE = 'S'
-FILE.DISAPPEARED_UNEXPECTEDLY = 'D'
-FILE.DELETED = 'X'
-FILE.NOT_EXISTS = 'T'
-FILE.NOT_YET_DELETED = 'E'
-FILE.NO_ALTERATION = '0'
-FILE.ALTERATION = '1'
-FILE.CONTENT_ALTERATION = '2'
-FILE.SIZE_ALTERATION = '3'
-FILE.CHANGE_TIME_ALTERATION = '4'           --CTIME
-FILE.MODIFICATION_TIME_ALTERATION = '5'     --MTIME
-FILE.INODE_ALTERATION = '6'
-FILE.OWNER_ALTERATION = '7'
-FILE.GROUP_ALTERATION = '8'
+FILE.NOT_YET_CREATED = '_'                  --The file has not yet been created
+FILE.FILE_PATTERN = 'P'                     --This is a file pattern
+FILE.HAS_BEEN_CREATED = 'C'                 --The file has been created
+FILE.IS_NOT_NOVELTY = 'N'                   --The file is not an expected novelty
+FILE.UNSTABLE_SIZE = 'U'                    --The file has an unstable file size
+FILE.UNEXPECTED_SIZE = 'S'                  --The file size is unexpected
+FILE.DISAPPEARED_UNEXPECTEDLY = 'D'         --The file has disappeared unexpectedly
+FILE.DELETED = 'X'                          --The file has been deleted
+FILE.NOT_EXISTS = 'T'                       --The file does not exist
+FILE.NOT_YET_DELETED = 'E'                  --The file has not been deleted yet
+FILE.NO_ALTERATION = '0'                    --The file has not been modified
+FILE.ALTERATION = '1'                       --The file has been modified
+FILE.CONTENT_ALTERATION = '2'               --The content of the file has been altered
+FILE.SIZE_ALTERATION = '3'                  --The file size has been altered
+FILE.CHANGE_TIME_ALTERATION = '4'           --The ctime of the file has been altered
+FILE.MODIFICATION_TIME_ALTERATION = '5'     --The mtime of the file has been altered
+FILE.INODE_ALTERATION = '6'                 --The number of inodes has been altered
+FILE.OWNER_ALTERATION = '7'                 --The owner of the file has changed
+FILE.GROUP_ALTERATION = '8'                 --The group of the file has changed
 
 return {
     FILE = FILE,
