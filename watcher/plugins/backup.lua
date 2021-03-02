fdc3198494ec512e586afddd0dbc53bb6ceeddc9
racherb <racherb@protonmail.com>
2021-03-01 21:23:53 -0300

    Add example case
    Add example case

---
+++ watcher/plugins/backup.lua
@@ -0,0 +1,25 @@
+#!/usr/bin/env tarantool
+------------
+-- Backup Plugins
+-- ...
+-- @module Backup
+-- @author Raciel Hernandez
+-- @license MIT
+-- @copyright 2021
+
+local fw = require('watcher').file
+
+-- Genera un backup de los archivos del watcher
+local function backup()
+    return true
+end
+
+--
+local function restore()
+    return true
+end
+
+return {
+    backup = backup,
+    restore = restore
+}
