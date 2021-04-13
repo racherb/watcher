fca5d73f16174bdcdf160762bda6d962f209df7d
racherb <racherb@protonmail.com>
2021-04-13 01:11:58 -0400

    Style normalization
    Style normalization

--- src/db/engine.lua
+++ src/db/engine.lua
@@ -9,6 +9,7 @@
 
 local strict = require('strict')
 local clock = require('clock')
+local errno = require('errno')
 local log = require('log')
 
 strict.on()
@@ -21,6 +22,7 @@ local FILE = require('types.file').FILE
 local WATCHER = require('types.file').WATCHER
 
 local function create_spaces()
+
     if not pcall(box.schema.create_space, 'awatcher') then
         return false
     else
@@ -32,11 +34,13 @@ local function create_spaces()
         }
     )
     end
+
     if not pcall(box.schema.create_space, 'watchables') then
         return false
     else
         log.info('The watchables scheme has been successfully created')
-        box.space.watchables:create_index('wat_uk',
+        box.space.watchables:create_index(
+            'wat_uk',
             {
                 type = 'tree',
                 parts = {{1, 'unsigned'}, {2, 'str'}},
@@ -51,11 +55,15 @@ local function create_spaces()
             }
         )
     end
+
     return true
+
 end
 
 local function start()
+
     box.cfg{}
+
     box.once('init', function()
         local ok = create_spaces()
         if not ok then
@@ -66,6 +74,7 @@ local function start()
 
     --User spaces initialization
     local plugin = require('plugins.default')
+
     box.once('init_default', function()
         local ok = plugin.user_spaces_def()
         if not ok then
@@ -85,7 +94,10 @@ local function wig()
 end
 
 -- Register a new watcher
-local function new(what, kind)
+local function new(
+    what,
+    kind
+)
     local id = wig()
 
     --New active watcher definition
@@ -94,7 +106,8 @@ local function new(what, kind)
         type = kind,
         what = what,
         dini = clock.realtime64(),
-        dend = 0
+        dend = 0,
+        answ = ''
         }
 
     local ok, tuple = awa.flatten(nawa)
@@ -119,11 +132,12 @@ local function add(
     message
 )
     local the_watcher = get(wid)
+
     -- Subscribe if wid exist and not finish yet
     if the_watcher and the_watcher[5]==0 then
         local _answer = answer or false
         local _message = message or FILE.NOT_YET_CREATED --'FILE_NOT_CREATED_YET'
-        --FILE.IS_PATTERN
+
         local watchb = {
             wid = wid,
             obj = object,
@@ -146,9 +160,13 @@ local function add(
     end
 end
 
-local function put(wid, object, ans, msg)
-    --local _msg = msg or FILE.HAS_BEEN_CREATED
+local function put(
+    wid,
+    object,
+    ans,msg
+)
     local the_watcher = get(wid)
+
     -- Subscribe if wid exist and not finish yet
     if the_watcher and the_watcher[5]==0 then
         local dreg = clock.realtime64()
@@ -174,13 +192,19 @@ local function put(wid, object, ans, msg)
     end
 end
 
-local function del(wid)
+local function del(
+    wid
+)
+
     local s = box.space.watchables.index.wat_uk
     local sel = s:select(wid)
+
     for _,v in pairs(sel) do
         s:delete({wid, v[3]})
     end
+
     box.space.awatcher:delete(wid)
+
 end
 
 -- Truncate watcher table
@@ -189,9 +213,17 @@ local function trun()
     box.space.awatcher:truncate()
 end
 
-local function upd(wid, object, ans, msg)
+local function upd(
+    wid,
+    object,
+    ans,
+    msg
+)
     box.space.watchables.index.wat_uk:update(
-        {wid, object},
+        {
+            wid,
+            object
+        },
         {
             {'=', 4, ans},
             {'=', 5, msg},
@@ -200,46 +232,83 @@ local function upd(wid, object, ans, msg)
     )
 end
 
-local function match(wid, wtype)
+local function match(
+    wid,
+    wtype
+)
     local _wtype = wtype or WATCHER.FILE_CREATION --File Watcher Creation is Default
+    local s = box.space.watchables.index.wat_ak_mssg
+
     if _wtype == WATCHER.FILE_CREATION then
-        return box.space.watchables.index.wat_ak_mssg:count(
-            {wid, FILE.HAS_BEEN_CREATED}
+        return s:count(
+            {
+                wid,
+                'C' --FILE.HAS_BEEN_CREATED
+            }
         )
     elseif _wtype == WATCHER.FILE_DELETION then
-        return box.space.watchables.index.wat_ak_mssg:count(
-            {wid, FILE.DELETED}
+        return s:count(
+            {
+                wid,
+                'X' --FILE.DELETED
+            }
         )
     else
-        return box.space.watchables.index.wat_ak_mssg:count(
-            {wid, wtype}
+        return s:count(
+            {
+                wid,
+                wtype
+            }
         )
     end
 end
 
-local function stat(wid)
+local function stat(
+    wid
+)
+
+    local sel = box.space.watchables.index.wat_ak_mssg
+    
     return {
         total = box.space.watchables:len(),
-        match = box.space.watchables.index.wat_ak_mssg:count(
-            {wid, FILE.HAS_BEEN_CREATED}
+        match = sel:count(
+            {
+                wid,
+                FILE.HAS_BEEN_CREATED
+            }
         )
     }
 end
 
 -- Close and end watcher wid
-local function endw(wid, dmatch, wtype)
+local function endw(
+    wid,
+    dmatch,
+    wtype
+)
+
     local t_watcher = get(wid)
+    local result
+
+    if match(wid, wtype) >= dmatch then
+        result = true
+    else
+        result = false
+    end
+
     if t_watcher then
         local v_end = clock.realtime64()
         box.space.awatcher:update(
-            wid, {{'=', 5, v_end}}
+            wid,
+            {
+                {'=', 5, v_end},
+                {'=', 6, result}
+            }
         )
     end
-    if match(wid, wtype) >= dmatch then
-        return true
-    else
-        return false
-    end
+
+    return result
+
 end
 
 local awatcher = {
