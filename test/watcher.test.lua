03c93cb6ac04fd7646cb28542a3d8f4b1f46498c
racherb <racherb@protonmail.com>
2021-04-21 00:29:33 -0400

    Refactoring tests
    Refactoring tests

--- test/watcher.test.lua
+++ test/watcher.test.lua
@@ -1,132 +1,120 @@
 #!/usr/bin/env tarantool
 
-package.path = package.path .. ';../watcher/src/?.lua;../src/?.lua;?.lua;../test/?.lua'
+package.path = package.path .. ';../watcher/src/?.lua;../src/?.lua;?.lua;../test/?.lua;test/?.lua'
 
 local tap = require('tap')
 local fiber = require('fiber')
 local helper = require('helper')
-local test = tap.test('test-file-watcher')
 
 local fwt = require('watcher').file
+local mon = require('watcher').monit
+local core = require('watcher').core
 
+local wat, res, prop, n_match, etime
+
+local MAXWAIT
+local INTERVAL
+
+local pini = os.time()
+test = tap.test('test-file-watcher')
 test:plan(6)
 
-local res
-local n_match
-local pini = os.time()
-
 --Plan 1
-test:test('Single File Deletion -> File does not exist', function(t)
-
-    local TEST = {
-        FWD01 = 'Nothing for watch',
-        FWD02 = "The file don't exist",
-        FWD03 = 'No wait if file not exist',
-        FWD04 = "The path don't exist"
-    }
+test:test('Single File Deletion >> The File Does Not Exist', function(t)
 
     t:plan(4)
 
     --FWD01
-    res = pcall(fwt.deletion, {''})
-    t:is(res, false, TEST.FWD01)
+    wat = pcall(fwt.deletion, {''})
+    t:is(wat, false, 'Nothing for watch')
 
     --FWD02
     local file_not_exist_yet = '_THIS.NOT$_?EXIST%'
-    res = fwt.deletion({file_not_exist_yet})
-    t:is(res.ans, true, TEST.FWD02)
+    wat = fwt.deletion({file_not_exist_yet}).wid
+    fiber.sleep(1)
+    res = mon.info(wat).ans
+    t:is(res, true, "The file don't exist")
 
     --FWD03
-    local MAXWAIT = 5
+    MAXWAIT = 7
     local tini = os.time()
-    local _ = fwt.deletion({'/tmp/' .. file_not_exist_yet}, MAXWAIT)
-    local elapsed_time = os.difftime(os.time() - tini)
-    t:ok(elapsed_time < MAXWAIT, TEST.FWD03)
+    local _ = core.waitfor(
+        fwt.deletion({'/tmp/' .. file_not_exist_yet}, MAXWAIT
+    ).wid)
+    etime = os.difftime(os.time() - tini)
+    t:ok(etime < MAXWAIT, 'No wait if file not exist')
 
     --FWD04
     local folder_not_exist_yet = '/tmp/THIS_FOLDER_NOT_EXIST/'
-    res = fwt.deletion({folder_not_exist_yet})
-    t:is(res.ans, true, TEST.FWD04)
+    wat = fwt.deletion({folder_not_exist_yet}).wid
+    fiber.sleep(1)
+    res = mon.info(wat).ans
+    t:is(res, true, "The path don't exist")
 end)
 
 --Plan 2
-test:test('Single File Deletion -> File exists but is not deleted', function(t)
-
-    local TEST = {
-        FWD01 = 'The file has not been deleted',
-        FWD02 = 'File not deleted in the maxwait interval'
-    }
-
+test:test('Single File Deletion >> File Exists But Is Not Deleted', function(t)
     t:plan(2)
 
     --FWD05
-    local MAXWAIT = 10
+    MAXWAIT = 5
     local this_file_exist = os.tmpname()
-    res = fwt.deletion({this_file_exist}, MAXWAIT)
-    t:is(res.ans, false, TEST.FWD01)
+    wat = fwt.deletion({this_file_exist}, MAXWAIT).wid
+    fiber.sleep(MAXWAIT+1)
+    res = mon.info(wat).ans
+    t:is(res, false, 'The file has not been deleted')
 
     --FWD06
-    MAXWAIT = 5
-    fiber.create(helper.remove_file, this_file_exist, 10)
-    res = fwt.deletion({this_file_exist}, MAXWAIT)
-    t:is(res.ans, false, TEST.FWD02)
+    MAXWAIT = 3
+    local this_file_exist_too = os.tmpname()
+    wat = fwt.deletion({this_file_exist_too}, MAXWAIT).wid
+    fiber.create(helper.remove_file, this_file_exist_too, MAXWAIT + 2)
+    fiber.sleep(MAXWAIT + 2)
+    res = mon.info(wat).ans
+    t:is(res, false, 'File not deleted in the maxwait interval')
 end)
 
 --Plan 3
-test:test('Single File Deletion -> File exists and is deleted', function(t)
-
-    local TEST = {
-        FWD01 = 'The file has been deleted'
-    }
-
+test:test('Single File Deletion >> File Exists And Is Deleted', function(t)
     t:plan(1)
 
     --FWD07
-    local MAXWAIT = 8
+    MAXWAIT = 5
     local this_file_exist = os.tmpname()
-    fiber.create(helper.remove_file, this_file_exist, 5)
-    res = fwt.deletion({this_file_exist}, MAXWAIT)
-    t:is(res.ans, true, TEST.FWD01)
+    fiber.create(helper.remove_file, this_file_exist, 3)
+    wat = fwt.deletion({this_file_exist}, MAXWAIT).wid
+    fiber.sleep(4)
+    res = mon.info(wat).ans
+    t:is(res, true, 'The file has been deleted')
 end)
 
 --Plan 4
-test:test('Multiple File Deletion -> Some varied experiments', function(t)
-
-    local TEST = {
-        FWD01 = 'No file on the list has been removed',
-        FWD02 = 'All files have been deleted',
-        FWD03 = 'Some items on the list have not been removed',
-        FWD04 = 'The number of cases has been eliminated',
-        FWD05 = 'No pattern elements have been removed',
-        FWD06 = 'All the pattern files have been deleted',
-        FWD07 = 'The folder has been deleted',
-        FWD08 = 'Combine lists and patterns together',
-        FWD09 = 'Match first 2 from 3 fst items ordered by change alpha asc',
-        FWD10 = 'Match first 2 from 3 fst items ordered by change alpha dsc',
-        FWD11 = 'Match first 2 from 3 fst items ordered by change mtime asc',
-        FWD12 = 'Match first 2 from 3 fst items ordered by change mtime dsc',
-        FWD13 = 'Match first 2 from 3 fst items ordered by change mtime dsc'
-    }
+test:test('Multiple File Deletion >> Some varied experiments', function(t)
 
     t:plan(13)
 
     --FWD08
-    local MAXWAIT = 3
-    local INTERVAL = 0.5
+    MAXWAIT = 3
+    INTERVAL = 0.5
     local f1 = os.tmpname()
     local f2 = os.tmpname()
     local f3 = os.tmpname()
     local file_list = {f1, f2, f3}
-    local res_g = fwt.deletion(file_list, MAXWAIT, INTERVAL)
-    t:is(res_g.ans, false, TEST.FWD01)
+    wat = fwt.deletion(file_list, MAXWAIT, INTERVAL)
+    fiber.sleep(MAXWAIT+1)
+    res = mon.info(wat.wid)
+    prop = (not res.ans) and (res.nomatch == #file_list)
+    t:is(prop, true, 'No file on the list has been removed')
 
     --FWD09
-    MAXWAIT = 10
+    MAXWAIT = 5
     fiber.create(helper.remove_file, f1, 1.5)
     fiber.create(helper.remove_file, f2, 1.5)
     fiber.create(helper.remove_file, f3, 1.5)
-    local res_h = fwt.deletion(file_list, MAXWAIT, INTERVAL, {nil, nil, 3})
-    t:is(res_h.ans, true, TEST.FWD02)
+    wat = fwt.deletion(file_list, MAXWAIT, INTERVAL, {nil, nil, 3})
+    fiber.sleep(3*1.5 + 1)
+    res = mon.info(wat.wid)
+    t:is(res.ans, true, 'All files have been deleted')
 
     --FWD10
     local f4 = os.tmpname()
@@ -136,38 +124,52 @@ test:test('Multiple File Deletion -> Some varied experiments', function(t)
     local f8 = os.tmpname()
     local f9 = os.tmpname()
     local file_list_2 = {f4, f5, f6, f7, f8, f9}
+    wat = fwt.deletion(file_list_2, MAXWAIT, INTERVAL)
     fiber.create(helper.remove_file, f4, 1)
-    fiber.create(helper.remove_file, f5, 3)
-    local res_i = fwt.deletion(file_list_2, MAXWAIT, INTERVAL)
-    t:is(res_i.ans, false, TEST.FWD03)
+    fiber.create(helper.remove_file, f5, 1)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    prop = (not res.ans) and (res.match == 2) and (res.nomatch == 4)
+    t:is(prop, true, 'Some items on the list have not been removed')
 
     --FWD11
-    MAXWAIT = 10
+    MAXWAIT = 4
     local file_list_3 = {f6, f7, f8, f9}
     fiber.create(helper.remove_file, f6, 1)
     fiber.create(helper.remove_file, f7, 1)
     fiber.create(helper.remove_file, f9, 1)
-    local res_j = fwt.deletion(file_list_3, MAXWAIT, INTERVAL, {nil, nil, 3})
-    t:is(res_j.ans, true, TEST.FWD04)
+    wat = fwt.deletion(file_list_3, MAXWAIT, INTERVAL, {nil, nil, 3})
+    fiber.sleep(MAXWAIT)
+    res = mon.info(wat.wid)
+    prop = (res.ans) and (res.match == 3) and (res.nomatch == 1)
+    t:is(prop, true, 'The number of cases has been eliminated')
 
     --FWD12
+    helper.remove_tmp_files(0)
+    fiber.sleep(1)
     for _=1,10 do os.tmpname() end
-    local file_pattern = {'/tmp/*'}
-    local res_k = fwt.deletion(file_pattern, MAXWAIT, INTERVAL)
-    t:is(res_k.ans, false, TEST.FWD05)
+    local file_pattern = {'/tmp/lua_*'}
+    wat = fwt.deletion(file_pattern, MAXWAIT, INTERVAL)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    prop = (not res.ans) and (res.match == 0)
+    t:is(prop, true, 'No pattern elements have been removed')
 
     --FWD13
-    MAXWAIT = 15
+    MAXWAIT = 6
     os.execute('touch /tmp/FAD_aaaaa')
     os.execute('touch /tmp/FAD_abaaa')
     os.execute('touch /tmp/FAD_acaaa')
     os.execute('touch /tmp/FAD_adaaa')
-    fiber.create(helper.remove_file, '/tmp/FAD_aaaaa', 2)
-    fiber.create(helper.remove_file, '/tmp/FAD_abaaa', 2)
-    fiber.create(helper.remove_file, '/tmp/FAD_acaaa', 2)
-    fiber.create(helper.remove_file, '/tmp/FAD_adaaa', 2)
-    res = fwt.deletion({'/tmp/FAD_*'}, MAXWAIT, INTERVAL)
-    t:is(res.ans, true, TEST.FWD06)
+    wat = fwt.deletion({'/tmp/FAD_*'}, MAXWAIT, INTERVAL)
+    fiber.create(helper.remove_file, '/tmp/FAD_aaaaa', 1)
+    fiber.create(helper.remove_file, '/tmp/FAD_abaaa', 1)
+    fiber.create(helper.remove_file, '/tmp/FAD_acaaa', 1)
+    fiber.create(helper.remove_file, '/tmp/FAD_adaaa', 1)
+    fiber.sleep(MAXWAIT)
+    res = mon.info(wat.wid)
+    prop = (res.ans) and (res.match == 4)
+    t:is(prop, true, 'All the pattern files have been deleted')
 
     --FWD14
     local folder = {'/tmp/thefolder'}
@@ -175,26 +177,29 @@ test:test('Multiple File Deletion -> Some varied experiments', function(t)
     os.execute('touch /tmp/thefolder/tst1.txt')
     os.execute('touch /tmp/thefolder/tst2.txt')
     os.execute('touch /tmp/thefolder/tst3.txt')
+    wat = fwt.deletion(folder, MAXWAIT, INTERVAL)
     fiber.create(helper.remove_tmp_folder, 3)
-    res = fwt.deletion(folder, MAXWAIT, INTERVAL)
-    t:is(res.ans, true, TEST.FWD07)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    prop = (res.ans) and (res.match == 1)
+    t:is(prop, true, 'The folder has been deleted')
 
     --FWD15
+    MAXWAIT = 5
     os.execute('touch /tmp/tst1.txt')
     os.execute('touch /tmp/tst2.txt')
     os.execute('touch /tmp/tst3.txt')
     os.execute('touch /tmp/tst4.txt')
     os.execute('touch /tmp/tst5.abc')
     os.execute('touch /tmp/tst6.abc')
+
     local watcher_mix = {'/tmp/tst*.txt', '/tmp/tst6.abc'}
-    local function remove_pattrn(pattrn, waitfor)
-        fiber.sleep(waitfor)
-        os.execute('rm -rf ' .. pattrn)
-    end
-    fiber.create(remove_pattrn, '/tmp/tst*.txt', 3)
+    wat = fwt.deletion(watcher_mix, MAXWAIT, INTERVAL)
+    fiber.create(helper.remove_pattrn, '/tmp/tst*.txt', 3)
     fiber.create(helper.remove_file, '/tmp/tst6.abc', 1)
-    res = fwt.deletion(watcher_mix, MAXWAIT, INTERVAL)
-    t:is(res.ans, true, TEST.FWD08)
+    fiber.sleep(MAXWAIT)
+    res = mon.info(wat.wid).ans
+    t:is(res, true, 'Combine lists and patterns together')
 
     --FWD16
     os.execute('touch /tmp/f_G.txt')
@@ -206,29 +211,35 @@ test:test('Multiple File Deletion -> Some varied experiments', function(t)
 
     n_match = 2
 
-    fiber.create(helper.remove_file, '/tmp/f_G.txt', 2)
-    fiber.create(helper.remove_file, '/tmp/f_L.txt', 2)
+    wat = fwt.deletion({'/tmp/f_*'}, MAXWAIT, INTERVAL, {'AA', 3, n_match})
+    fiber.create(helper.remove_file, '/tmp/f_G.txt', 1)
+    fiber.create(helper.remove_file, '/tmp/f_L.txt', 1)
     fiber.create(helper.remove_file, '/tmp/f_K.txt', 2)
-    res = fwt.deletion({'/tmp/f_*'}, MAXWAIT, INTERVAL, {'AA', 3, n_match})
-    t:is(res.ans, false, TEST.FWD09)
+    fiber.sleep(MAXWAIT)
+    res = mon.info(wat.wid)
+    prop = (res.ans) and (res.match >= 2) and (res.match <= 3)
+    t:is(prop, false, 'Match first 2 from 3 fst items ordered by change Alpha Asc')
 
     --FWD17
-    remove_pattrn('/tmp/f_*', 0)
+    helper.remove_pattrn('/tmp/f_*', 0)
     os.execute('touch /tmp/f_G.txt')
     os.execute('touch /tmp/f_H.txt')
     os.execute('touch /tmp/f_I.txt')
     os.execute('touch /tmp/f_J.txt')
     os.execute('touch /tmp/f_K.txt')
     os.execute('touch /tmp/f_L.txt')
-
-    fiber.create(helper.remove_file, '/tmp/f_G.txt', 2)
-    fiber.create(helper.remove_file, '/tmp/f_L.txt', 2)
-    fiber.create(helper.remove_file, '/tmp/f_K.txt', 2)
-    res = fwt.deletion({'/tmp/f_*'}, MAXWAIT, INTERVAL, {'AD', 3, n_match})
-    t:is(res.ans, true, TEST.FWD10)
+    wat = fwt.deletion({'/tmp/f_*'}, MAXWAIT, INTERVAL, {'AD', 3, n_match})
+    fiber.create(helper.remove_file, '/tmp/f_G.txt', 1)
+    fiber.create(helper.remove_file, '/tmp/f_L.txt', 1.3)
+    fiber.create(helper.remove_file, '/tmp/f_K.txt', 1.5)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    prop = (res.ans) and (res.match >= 2) and (res.match <= 3) and (res.nomatch == 1)
+    t:is(res.ans, true, 'Match first 2 from 3 fst items ordered by change Alpha Dsc')
 
     --FWD18
-    remove_pattrn('/tmp/f_*', 0)
+    helper.remove_pattrn('/tmp/f_*', 0)
+    fiber.sleep(1)
     os.execute('touch /tmp/f_1.txt')
     fiber.sleep(1)
     os.execute('touch /tmp/f_2.txt')
@@ -236,114 +247,131 @@ test:test('Multiple File Deletion -> Some varied experiments', function(t)
     os.execute('touch /tmp/f_3.txt')
     fiber.sleep(1)
     os.execute('touch /tmp/f_4.txt')
+    fiber.sleep(1)
 
+    MAXWAIT = 5
+    wat = fwt.deletion({'/tmp/f_*'}, MAXWAIT, INTERVAL, {'MA', 3, n_match})
     fiber.create(helper.remove_file, '/tmp/f_1.txt', 2)
-    fiber.create(helper.remove_file, '/tmp/f_3.txt', 2)
-    res = fwt.deletion({'/tmp/f_*'}, MAXWAIT, INTERVAL, {'MA', 3, n_match})
-    t:is(res.ans, true, TEST.FWD11)
+    fiber.create(helper.remove_file, '/tmp/f_3.txt', 1)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    prop = (res.ans) and (res.match >= 2) and (res.match <= 3)
+    t:is(prop, true, 'Match first 2 from 3 fst items ordered by change Mtime Asc')
 
     --FWD19
-    remove_pattrn('/tmp/f_*', 0)
+    helper.remove_pattrn('/tmp/f_*', 0)
     os.execute('touch /tmp/f_1.txt')
+    fiber.sleep(0.8)
     os.execute('touch /tmp/f_2.txt')
+    fiber.sleep(1)
     os.execute('touch /tmp/f_3.txt')
+    fiber.sleep(0.5)
     os.execute('touch /tmp/f_4.txt')
 
-    fiber.create(helper.remove_file, '/tmp/f_1.txt', 2)
-    fiber.create(helper.remove_file, '/tmp/f_3.txt', 2)
-    res = fwt.deletion({'/tmp/f_*'}, MAXWAIT, INTERVAL, {'MD', 3, n_match})
-    t:is(res.ans, false, TEST.FWD12)
+    wat = fwt.deletion({'/tmp/f_*'}, MAXWAIT, INTERVAL, {'MD', 3, n_match})
+    fiber.create(helper.remove_file, '/tmp/f_2.txt', 1)
+    fiber.create(helper.remove_file, '/tmp/f_3.txt', 1.5)
+    fiber.sleep(MAXWAIT + 3)
+    res = mon.info(wat.wid)
+    prop = (res.ans) and (res.match >= 2) and (res.match <= 3)
+    t:is(prop, true, 'Match first 2 from 3 fst items ordered by change Mtime Dsc')
 
     --FWD20
-    remove_pattrn('/tmp/f_*', 0)
+    helper.remove_pattrn('/tmp/f_*', 0)
+    fiber.sleep(2)
     os.execute('touch /tmp/f_1.txt')
     os.execute('touch /tmp/f_2.txt')
     os.execute('touch /tmp/f_3.txt')
     os.execute('touch /tmp/f_4.txt')
 
-    fiber.create(helper.remove_file, '/tmp/f_2.txt', 2)
-    fiber.create(helper.remove_file, '/tmp/f_3.txt', 2)
-    res = fwt.deletion({'/tmp/f_*'}, MAXWAIT, INTERVAL, {'MD', 3, n_match})
-    t:is(res.ans, true, TEST.FWD13)
-
-    --test:is(#obj, n_match, TEST.FWD18)
+    wat = fwt.deletion({'/tmp/f_*'}, MAXWAIT, INTERVAL, {'NS', 3, n_match})
+    fiber.create(helper.remove_file, '/tmp/f_1.txt', 1)
+    fiber.create(helper.remove_file, '/tmp/f_4.txt', 1)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    prop = (not res.ans) and (res.match == 1) and (res.nomatch == 2)
+    t:is(prop, true, 'Match only one of 3 las cases no sorted')
 
 end)
 
 --Plan 5
 test:test('Single File Creation', function(t)
 
-    local TEST = {
-        FWD01 = 'The file already existed',
-        FWD02 = 'The file has arrived!',
-        FWD03 = 'The file has not been created'
-    }
-
     t:plan(3)
 
-    local MAXWAIT = 4
-    local INTERVAL = 0.5
+    MAXWAIT = 3
+    INTERVAL = 0.5
 
     --FWD21
     local c1 = os.tmpname()
-    fiber.sleep(2)
-    res = fwt.creation({c1}, MAXWAIT, INTERVAL)
-    t:is(res.ans, true, TEST.FWD01)
+    wat = fwt.creation({c1}, MAXWAIT, INTERVAL)
+    fiber.sleep(MAXWAIT)
+    res = mon.info(wat.wid)
+    t:is(res.ans, true, 'The file already existed')
 
     --FWD22
-    helper.create_file('/tmp/c_fdw022', 3)
-    res = fwt.creation({c1}, MAXWAIT, INTERVAL)
-    t:is(res.ans, true, TEST.FWD02)
+    wat = fwt.creation({c1}, MAXWAIT, INTERVAL)
+    helper.create_file('/tmp/c_fdw022', 2)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    t:is(res.ans, true, 'The file has arrived!')
 
     --FWD23
-    res = fwt.creation({'/tmp/c_fdw023'}, MAXWAIT, INTERVAL)
-    t:is(res.ans, false, TEST.FWD03)
+    wat = fwt.creation({'/tmp/c_f_d_w_0_2_3'}, MAXWAIT, INTERVAL)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    t:is(res.ans, false, 'The file has not been created')
+
+    helper.remove_pattrn('/tmp/c_*', 0)
 
 end)
 
 --Plan 6
 test:test('Advanced File Creation', function(t)
 
-    local TEST = {
-        FWD01 = 'The file size not expected',
-        FWD02 = 'The file size has arrived!',
-        FWD03 = '2000 files are expected and 2000 files arrive',
-        FWD04 = '2100 files are expected but only 2000 arrive',
-        FWD05 = 'The file has unexpectedly disappeared'
-    }
+    t:plan(4)
 
-    t:plan(5)
-
-    local MAXWAIT = 4
-    local INTERVAL = 0.5
+    MAXWAIT = 4
+    INTERVAL = 0.5
 
     local c1 = os.tmpname()
-    fiber.sleep(2)
-    res = fwt.creation({c1}, MAXWAIT, INTERVAL, 10)
-    t:is(res.ans, false, TEST.FWD01)
+    wat = fwt.creation({c1}, MAXWAIT, INTERVAL, 10)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    t:is(res.ans, false, 'The file size not expected')
 
     helper.append_file(c1, 2)
-    res = fwt.creation({c1}, MAXWAIT, INTERVAL, 10)
-    t:is(res.ans, true, TEST.FWD02)
+    wat = fwt.creation({c1}, MAXWAIT, INTERVAL, 10)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    t:is(res.ans, true, 'The file size has arrived!')
 
     --fiber.create(append_file, c1, 2)
 
     helper.remove_tmp_files(0)
+    wat = fwt.creation({'/tmp/lua_*'}, nil, nil, 0, nil, nil, 2000)
     fiber.create(helper.create_nfiles, 2000)
-    res = fwt.creation({'/tmp/lua_*'}, nil, nil, 0, nil, nil, 2000)
-    t:is(res.ans, true, TEST.FWD03)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    prop = (res.ans) and (res.match == 2000)
+    t:is(prop, true, '2000 files are expected and 2000 files arrive')
 
+    MAXWAIT = 5
     helper.remove_tmp_files(0)
+    wat = fwt.creation({'/tmp/lua_*'}, MAXWAIT, nil, 0, nil, nil, 2100)
     fiber.create(helper.create_nfiles, 2000)
-    res = fwt.creation({'/tmp/lua_*'}, 10, nil, 0, nil, nil, 2100)
-    t:is(res.ans, false, TEST.FWD04)
-    helper.remove_tmp_files(0)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    t:is(res.ans, false, '2100 files are expected but only 2000 arrive')
 
+    --[[helper.remove_tmp_files(0)
     local c2 = os.tmpname()
-    fiber.create(helper.remove_file, c2, 5)
-    res = fwt.creation({c2}, 10, INTERVAL, 10)
-    t:is(res.ans, false, TEST.FWD05)
-
+    wat = fwt.creation({c2}, 10, INTERVAL, 10)
+    fiber.create(helper.remove_file, c2, 0)
+    fiber.sleep(MAXWAIT + 1)
+    res = mon.info(wat.wid)
+    t:is(res.ans, false, 'The file has unexpectedly disappeared')
+    --]]
 end)
 
 print('Elapsed time: ' .. os.difftime(os.time() - pini) .. 's')
@@ -353,5 +381,3 @@ os.exit(test:check() == true and 0 or -1)
 return {
     test = test
 }
-
---]]
