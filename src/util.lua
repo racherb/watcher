5c5dd64f3000cf514679ca354d5d8f2c3f05340e
racherb <racherb@protonmail.com>
2022-03-15 23:42:57 -0300

    Add comment
    Add comment

--- src/util.lua
+++ src/util.lua
@@ -28,6 +28,7 @@ local function val_to_str(v)
     end
   end
 
+  --Lua table to string format
   local function tostring(tbl)
     local result, done = {}, {}
     for k, v in ipairs(tbl) do
@@ -51,6 +52,21 @@ local function is_val_of(tbl, value)
     end
   end
   return false
+end
+
+--Determines when a string belongs to the given pattern
+local function is_pat_of(tbl, pattrn)
+    --local is_pattrn = string.find(pattrn, '*')
+    --if not is_pattrn then return false end
+    tbl[0] = #tbl
+    for i=1,tbl[0] do
+      local from_f = (tbl[i]):find(pattrn)
+      --local from_s = (pattrn):find(tbl[i])
+      if from_f == 1 then
+        return true
+      end
+    end
+    return false
 end
 
 -- Determines if a key exists in a given table
@@ -83,6 +99,7 @@ local util = {
   tostring = tostring,
   is_val_of = is_val_of,
   is_key_of = is_key_of,
+  is_pat_of = is_pat_of,
   deduplicate = remove_duplicates
 }
 
